8403379a6ad2faa6a9d2d3fb48ac1e742f210561
chrialge <chrialge@gmail.com>
2024-09-24 00:50:34 +0200

    add crud operation for destroy, update and half for store

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Str;
+use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -34,7 +38,20 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $val_data = $request->validate([
+            'name' => 'required|max:100',
+            'image' => 'nullable|image',
+            'price' => 'required|between:0.00,99999.99',
+            'availability' => 'nullable|boolean',
+            'color' => 'nullable|max: 50',
+            'description' => 'nullable|text'
+        ]);
+
+        return response()->json([
+            'success' => 'true',
+            'response' => $val_data
+        ]);
     }
 
     /**
@@ -65,11 +82,103 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $check_product = Product::where('id', $id)->exists();
+
+        if ($check_product) {
+            $product = Product::where('id', $id)->first();
+
+
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:100',
+                'image' => 'nullable|image',
+                'price' => 'required|between:0.00,99999.99',
+                'availability' => 'nullable|boolean',
+                'color' => 'nullable|max: 50',
+                'description' => 'nullable|text'
+            ], [
+                'name.required' => 'required name',
+                'name.max' => 'the length of the name is :max',
+                'image.image' => 'accepted only file jpg, jpeg, png',
+                'price.required' => 'required price',
+                'price.between' => 'The :attribute value :input is not between :min - :max.',
+                'color.max' => 'the length of the color is :max',
+                'description.text' => 'description only acapted string'
+            ]);
+
+
+
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'response' => $validator->errors(),
+                ]);
+            } else {
+
+                $val_data = $validator->validated();
+
+
+
+                if ($request->has('image')) {
+                    $val_data['image'] = Storage::disk('public')->put('uploads/images', $val_data['image']);
+                }
+
+                $check_slug = Product::where('name', $val_data['name'])->count();
+                $slug = "";
+
+                if ($check_slug > 0) {
+                    $slug = Str::slug($val_data['name'], '-') . "-$check_slug";
+                } else {
+                    $slug = Str::slug($val_data['name'], '-');
+                }
+                $val_data['slug'] = $slug;
+
+                // return response()->json([
+                //     'success' => true,
+                //     'response' =>  $val_data,
+
+                // ]);
+
+                $product->update($val_data);
+
+                return response()->json([
+                    'success' => true,
+                    'response' =>  "the $product->name data have been successfully updated",
+
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'response' => "the products don't exist"
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id) {}
+    public function destroy(string $id)
+    {
+
+        // controlle se l'id esiste per product 
+        $check_product = Product::where('id', $id)->exists();
+
+        if ($check_product) {
+
+            $product = Product::where('id', $id)->first();
+
+            $product->delete();
+
+            return response()->json([
+                'success' => true,
+                'response' => "has been deleted successfully $product->name"
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'response' => "the products don't exist"
+            ]);
+        }
+    }
 }
